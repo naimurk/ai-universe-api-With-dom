e9e0b4735914cc8314ce0492d3ee36241228d4cd
naimurk <naimurk781@gmail.com>
2023-03-03 12:32:20 +0600

    ternory operator add

--- js/Ai.js
+++ js/Ai.js
@@ -75,7 +75,9 @@ const modalData = (id)=> {
     const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(URL)
     .then(res=> res.json())
-    .then(data=> displayModal(data.data))
+    .then(data=>{ 
+        // console.log(data)
+        displayModal(data.data)})
 }
 
 const displayModal = (element) => {
@@ -87,15 +89,15 @@ const displayModal = (element) => {
    <div class="flex flex-col lg:flex-row gap-4 my-5 items-center">
        <div class="py-12 px-3 rounded shadow-xl bg-white">
        <p>${pricing[0].plan}</p>
-       <p>${pricing[0].price}</p>
+       <p>${pricing[0].price  ? pricing[0].price : 'free of cost'}</p>
        </div>
        <div class="py-12 px-3 rounded shadow-xl bg-white">
        <p>${pricing[1].plan}</p>
-       <p>${pricing[1].price}</p>
+       <p>${pricing[1].price ? pricing[1].price : 'free of cost' }</p>
        </div>
        <div class="py-12 px-3 rounded shadow-xl bg-white">
        <p>${pricing[2].plan}</p>
-       <p>${pricing[2].price}</p>
+       <p>${pricing[2].price ? pricing[2].price : 'free of cost'}</p>
        </div>
       
    </div>
@@ -103,25 +105,25 @@ const displayModal = (element) => {
        <div>
            <h1 class="text-2xl  font-bold" >Features</h1>
            <ul class=" py-3">
-               <li>${features['1'].feature_name}</li>
-               <li>${features['2'].feature_name}</li>
-               <li>${features['3'].feature_name}</li>
+               <li>${features['1'].feature_name ? features['1'].feature_name : 'feature no found' }</li>
+               <li>${features['2'].feature_name ? features['2'].feature_name : 'feature no found' }</li>
+               <li>${features['3'].feature_name ? features['3'].feature_name : 'feature no found' }</li>
            </ul>
        </div>
        <div class="ml-5">
            <h1 class="text-2xl  font-bold">Integration</h1>
            <ul class=" py-3">
-               <li>${integrations[0]}</li>
-               <li>${integrations[1]}</li>
-               <li>${integrations[2]}</li>
+               <li>${integrations[0] ? integrations[0] : 'no data found'}</li>
+               <li>${integrations[1] ? integrations[1] : 'no data found'}</li>
+               <li>${integrations[2] ? integrations[2] : 'no data found'}</li>
            </ul>
        </div>
    </div>
 </div>
 <div class="w-full lg:w-1/2 border flex flex-col justify-center text-center rounded-md px-5 py-5 lg:px-12 lg:py-12 ">
    <img class="h-80" src="${image_link[0]}" alt="">
-   <h1 class="text-2xl  font-bold py-5" >${input_output_examples[0].input}</h1>
-   <p>${input_output_examples[0].output}</p>
+   <h1 class="text-2xl  font-bold py-5" >${input_output_examples[0].input ? input_output_examples[0].input : 'question not found'}</h1>
+   <p>${input_output_examples[0].output ? input_output_examples[0].output : 'No! Not Yet! Take a break!!!'}</p>
 </div>
    `;
    
